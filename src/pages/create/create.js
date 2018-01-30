b315533266b060d5b822113ed829c0a036e9c51e
liuqixing <liuqixing@dekuan.org>
2018-01-30 13:04:50 +0800

    gogogo

--- src/pages/create/create.js
+++ src/pages/create/create.js
@@ -3,7 +3,9 @@
 //	获取应用实例
 //
 var wurl		= require( '../../libs/wurl.js' );
+var wlib		= require( '../../libs/wlib.js' );
 const app		= getApp();
+
 
 
 Page({
@@ -24,6 +26,24 @@ Page({
 
 	onFormCreateSubmit: function( oEvent )
 	{
+		var sMessage;
+		var sPassword;
+		var sPasswordHint;
+
+		if ( ! wlib.isObjectWithKeys( oEvent, 'detail' ) ||
+			! wlib.isObjectWithKeys( oEvent.detail, 'value' ) ||
+			! wlib.isObjectWithKeys( oEvent.detail.value, [ 'message', 'password', 'password_hint' ] ) )
+		{
+
+		}
+
+		//	...
+		sMessage		= '';
+		sPassword		= '';
+		sPasswordHint	= '';
+
+
+
 		console.log("onFormCreateSubmit", oEvent);
 
 		wx.navigateTo({
