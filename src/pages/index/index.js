a1e0ee023a31b48cebf116cd84a7d425e216da8c
liuqixing <liuqixing@dekuan.org>
2018-01-31 23:26:53 +0800

    clear up index

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,7 +3,6 @@
 //	获取应用实例
 //
 var wurl		= require( '../../libs/wurl.js' );
-
 const app		= getApp();
 
 
@@ -13,19 +12,6 @@ Page({
 
 	data:
 	{
-		motto: 'Hello World',
-		userInfo: {},
-		hasUserInfo: false,
-		canIUse: wx.canIUse('button.open-type.getUserInfo')
-	},
-
-	//	事件处理函数
-	bindViewTap : function()
-	{
-		wx.navigateTo
-		({
-			url: '../logs/logs'
-		});
 	},
 
 	onLoad: function ( oOptions )
@@ -36,54 +22,7 @@ Page({
 		console.log("wurl.getCurrentPageArgs() = ", wurl.getCurrentPageArgs());
 		console.log("wurl.getCurrentPageUrlWithArgs() = " + wurl.getCurrentPageUrlWithArgs());
 		console.log( "##########" );
-
-		if ( app.globalData.userInfo )
-		{
-			this.setData({
-				userInfo: app.globalData.userInfo,
-				hasUserInfo: true
-			});
-		}
-		else if ( this.data.canIUse )
-		{
-			//	由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-			//	所以此处加入 callback 以防止这种情况
-			app.userInfoReadyCallback = res => {
-				this.setData({
-					userInfo: res.userInfo,
-					hasUserInfo: true
-				});
-			};
-		}
-		else
-		{
-			// 在没有 open-type=getUserInfo 版本的兼容处理
-			wx.getUserInfo
-			({
-				success: res => {
-					app.globalData.userInfo = res.userInfo
-					this.setData
-					({
-						userInfo: res.userInfo,
-						hasUserInfo: true
-					});
-				}
-			});
-		}
 	},
-
-	getUserInfo : function( e )
-	{
-		console.log( e );
-		
-		app.globalData.userInfo = e.detail.userInfo;
-		this.setData
-		({
-			userInfo: e.detail.userInfo,
-			hasUserInfo: true
-		});
-	},
-
 
 	onShareAppMessage : function( res )
 	{
@@ -94,8 +33,8 @@ Page({
 		}
 
 		return {
-			title : '自定义转发标题',
-			path : '/pages/index/index?id=1230993030303030303&t=12121212121212121212&c=asdfKiekkdkdkadfadf0a234234dfasdflkjas019873alskdjfhalskdjfhalsdkjfhakljsdhfaklshdf',
+			title : '「加密纸条」',
+			path : '/pages/index/index?fp=share',
 			success : function( res )
 			{
 				// 转发成功
